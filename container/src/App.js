b3279f15857ac54d09eb04df3b6ad7013cc8f429
Wagdy Samih <WagdySamih787@gmail.com>
2025-01-13 00:19:11 +0200

    fix: css in js class name collusion fix - adds class namespace

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import Marketing from "./components/Marketing";
 import Header from "./components/Header";
+
+const generateClassname = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Marketing />
+      <StylesProvider generateClassName={generateClassname}>
+        <Header />
+        <Marketing />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
