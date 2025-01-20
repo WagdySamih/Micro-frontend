40c66af0adae276b01ebd3f4e1c8cab2d475e387
Wagdy Samih <WagdySamih787@gmail.com>
2025-01-20 20:34:47 +0200

    feat: adds custom progress bar

--- container/src/App.js
+++ container/src/App.js
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import Header from "./components/Header";
+import Progress from "./components/Progress";
 
 const Marketing = lazy(() => import("./components/Marketing"));
 const Auth = lazy(() => import("./components/Auth"));
@@ -15,7 +16,7 @@ const App = () => {
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassname}>
         <Header />
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<Progress />}>
           <Switch>
             <Route path="/auth" component={Auth} />
             <Route path="/" component={Marketing} />
